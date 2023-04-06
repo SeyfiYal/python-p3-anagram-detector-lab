f589807b701e4f3f58f354c82b433f04c231dfda
Seyfican Yalcindag <+>
2023-04-06 19:16:43 +0300

    Anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+
+class Anagram():
+    def __init__(self,word):
+        self.word = word
+
+    def match(self,word_list):
+        anagrams = []
+        sorted_word = sorted(self.word.lower())
+        for candidate in word_list:
+            if sorted(candidate.lower()) == sorted_word and candidate != self.word:
+                anagrams.append(candidate)
+        return anagrams
+
+
+
+listen = Anagram("listen")
+listen.match(['enlists', 'google', 'inlets', 'banana'])
